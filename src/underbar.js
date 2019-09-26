70b033ec0937f6658f91df8e990ecf10885e4892
JohnReiland <reiland.shanghai@gmail.com>
2019-09-25 23:55:18 -0400

    "Fix underbar map"

--- src/underbar.js
+++ src/underbar.js
@@ -168,10 +168,19 @@
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
+    var mapped = [];
+    for (var i = 0; i < collection.length; i++) {
+      mapped.push(iterator(collection[i]));
+    }
+    return mapped;
+  }; 
+
+
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
-  };
+    
+
 
   /*
    * TIP: map is really handy when you want to transform an array of
