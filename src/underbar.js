9cb860f5afa6ff1a15adec3f5ee114f201481a83
JohnReiland <reiland.shanghai@gmail.com>
2019-09-25 20:55:50 -0400

    "Fix underbar reject"

--- src/underbar.js
+++ src/underbar.js
@@ -109,21 +109,29 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
-    var filtered =[];
+    var filtered = [];
 
     for (var i = 0; i < collection.length; i++) {
       if (test(collection[i]) === true) {
         filtered.push(collection[i]);
       }
     }
-    
+
     return filtered;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
-    // TIP: see if you can re-use _.filter() here, without simply
-    // copying code in and modifying it
+    var rejected = [];
+    var filtered = _.filter(collection, test);
+
+    for (var i = 0; i < collection.length; i++) {
+      if (filtered.includes(collection[i]) === false) {
+        rejected.push(collection[i]);
+      }
+    }
+
+    return rejected;
   };
 
   // Produce a duplicate-free version of the array.
