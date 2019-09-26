d0c4205fd110ddd8aa498ab3c0a9b77bb122274e
JohnReiland <reiland.shanghai@gmail.com>
2019-09-26 14:10:01 -0400

    "fix underbar extend"

--- src/underbar.js
+++ src/underbar.js
@@ -335,6 +335,12 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i = 1; i < arguments.length; i++) {
+      for (var prop in arguments[i]) {
+        arguments[0][prop] = arguments[i][prop];
+      }
+    }
+    return arguments[0];
   };
 
   // Like extend, but doesn't ever overwrite a key that already
